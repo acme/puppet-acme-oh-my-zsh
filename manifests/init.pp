b8fb7bbe9cc68ea1d3e8daf8b1b5d62849c43f34
Leon Brocard <acme@astray.com>
2013-10-10 05:35:08 +0100

    Add documentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,39 +1,26 @@
 # == Class: oh-my-zsh
 #
-# Full description of class oh-my-zsh here.
+# This is the oh-my-zsh module. It installs oh-my-zsh for a user and changes
+# their shell to zsh. It has been tested under Ubuntu.
+#
+# oh-my-zsh is a community-driven framework for managing your zsh configuration.
 #
 # === Parameters
 #
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
+# None.
 #
 # === Examples
 #
-#  class { oh-my-zsh:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#  }
+# class { 'oh-my-zsh': }
+# oh-my-zsh::install { 'acme': }
 #
 # === Authors
 #
-# Author Name <author@domain.com>
+# Leon Brocard <acme@astray.com>
 #
 # === Copyright
 #
-# Copyright 2013 Your name here, unless otherwise noted.
+# Copyright 2013 Leon Brocard
 #
 class oh-my-zsh {
   if(!defined(Package['git'])) {
