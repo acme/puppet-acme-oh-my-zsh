f69d7bfb08c8109504f0f8986732c03d54a47e86
Leon Brocard <acme@astray.com>
2013-10-10 05:40:06 +0100

    Fix a couple of issues spotted by puppet-lint

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,22 +1,46 @@
+# == Class: oh-my-zsh::install
+#
+# This is the oh-my-zsh module. It installs oh-my-zsh for a user and changes
+# their shell to zsh. It has been tested under Ubuntu.
+#
+# oh-my-zsh is a community-driven framework for managing your zsh configuration.
+#
+# === Parameters
+#
+# None.
+#
+# === Examples
+#
+# class { 'oh-my-zsh': }
+# oh-my-zsh::install { 'acme': }
+#
+# === Authors
+#
+# Leon Brocard <acme@astray.com>
+#
+# === Copyright
+#
+# Copyright 2013 Leon Brocard
+#
 define oh-my-zsh::install() {
   exec { 'oh-my-zsh::git clone':
-    creates => "/home/$name/.oh-my-zsh",
-    command => "/usr/bin/git clone git://github.com/robbyrussell/oh-my-zsh.git /home/$name/.oh-my-zsh",
+    creates => "/home/${name}/.oh-my-zsh",
+    command => "/usr/bin/git clone git://github.com/robbyrussell/oh-my-zsh.git /home/${name}/.oh-my-zsh",
     user    => $name,
     require => [Package['git'], Package['zsh']]
   }
 
   exec { 'oh-my-zsh::cp .zshrc':
-    creates => "/home/$name/.zshrc",
-    command => "/bin/cp /home/$name/.oh-my-zsh/templates/zshrc.zsh-template /home/$name/.zshrc",
+    creates => "/home/${name}/.zshrc",
+    command => "/bin/cp /home/${name}/.oh-my-zsh/templates/zshrc.zsh-template /home/${name}/.zshrc",
     user    => $name,
     require => Exec['oh-my-zsh::git clone'],
   }
 
-  user { "oh-my-zsh::user $name":
-    name    => $name,
+  user { "oh-my-zsh::user ${name}":
     ensure  => present,
+    name    => $name,
     shell   => '/usr/bin/zsh',
-    require => Package['zsh'], 
+    require => Package['zsh'],
   }
 }
