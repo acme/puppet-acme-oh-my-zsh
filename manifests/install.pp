0ee8883d8269623f7c8c40046a3731e236efb794
Leon Brocard <acme@astray.com>
2014-04-08 20:28:41 +0100

    Put variables in {}

--- manifests/install.pp
+++ manifests/install.pp
@@ -26,19 +26,19 @@
 # Copyright 2013 Leon Brocard
 #
 define ohmyzsh::install() {
-  if $name == "root" { $home = "/root" } else { $home = "${ohmyzsh::params::home}/${name}" }
-  exec { "ohmyzsh::git clone $name":
+  if $name == 'root' { $home = '/root' } else { $home = "${ohmyzsh::params::home}/${name}" }
+  exec { "ohmyzsh::git clone ${name}":
     creates => "${home}/.oh-my-zsh",
     command => "/usr/bin/git clone git://github.com/robbyrussell/oh-my-zsh.git ${home}/.oh-my-zsh",
     user    => $name,
     require => [Package['git'], Package['zsh']]
   }
 
-  exec { "ohmyzsh::cp .zshrc $name":
+  exec { "ohmyzsh::cp .zshrc ${name}":
     creates => "${home}/.zshrc",
     command => "/bin/cp ${home}/.oh-my-zsh/templates/zshrc.zsh-template ${home}/.zshrc",
     user    => $name,
-    require => Exec["ohmyzsh::git clone $name"],
+    require => Exec["ohmyzsh::git clone ${name}"],
   }
 
   if ! defined(User[$name]) {
