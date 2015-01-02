643d43f7d2cd72963360eb980e646a157468e499
Zan Loy <zan.loy@gmail.com>
2015-01-02 08:07:51 -0500

    updated format for plugins, no reason to have user not equal name

--- manifests/plugins.pp
+++ manifests/plugins.pp
@@ -1,14 +1,20 @@
 define ohmyzsh::plugins(
   $plugins = 'git',
-  $user = $name
 ) {
-  if $user == 'root' { $home = '/root' } else { $home = "${ohmyzsh::params::home}/${user}" }
-  if $user {
-    file_line { "${user}-${plugins}-install":
+
+  if $name == 'root' {
+    $home = '/root'
+  } else {
+    $home = "${ohmyzsh::params::home}/${name}"
+  }
+
+  if $name {
+    file_line { "${name}-${plugins}-install":
       path    => "${home}/.zshrc",
       line    => "plugins=(${plugins})",
       match   => '^plugins=',
-      require => Ohmyzsh::Install[$user]
+      require => Ohmyzsh::Install[$name]
     }
   }
+
 }
--- manifests/vagrant.pp
+++
@@ -1,7 +0,0 @@
-class { 'ohmyzsh': }
-
-# for a single user
-ohmyzsh::install { 'vagrant': }->
-ohmyzsh::upgrade { 'vagrant': }
-
-ohmyzsh::install { ['one', 'two']: }
