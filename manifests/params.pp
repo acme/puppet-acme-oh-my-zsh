716a61ff9277490ce1c33496f32d23f61e95e273
Dennis de Greef <github@link0.net>
2015-10-17 16:27:29 +0200

    Add FreeBSD support in default parameters

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,9 @@
 class ohmyzsh::params {
   case $::operatingsystem {
+    /(?i:FreeBSD)/: {
+      $zsh = '/usr/local/bin/zsh'
+      $home = '/home'
+    }
     default: {
       $zsh = '/usr/bin/zsh'
       $home = '/home'
